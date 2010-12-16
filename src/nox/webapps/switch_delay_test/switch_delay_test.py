2a1b95fb358e45291f041ca8b02eb5391d9fa8d7
crotsos <cr409@cam.ac.uk>
2010-12-16 18:21:31 +0100

    fixing an error in the testing module

--- src/nox/webapps/switch_delay_test/switch_delay_test.py
+++ src/nox/webapps/switch_delay_test/switch_delay_test.py
@@ -22,7 +22,7 @@ import simplejson as json
 from nox.webapps.webservice import webservice
 from nox.coreapps.pyrt.pycomponent import Packet_in_event 
 from nox.lib import core, openflow, packet, util
-from nox.lib.packet import ethernet, ipv4
+from nox.lib.packet import ethernet, ipv4, packet_utils
 from nox.coreapps.pyrt.pycomponent import CONTINUE, STOP
 
 Switch_Delay_Test = None
@@ -68,8 +68,8 @@ def datapath_leave(dpid):
     """ Event handler for controller detection of datapath going down. """
     del  Switch_Delay_Test.st['ports'][dpid]
 
-def handler(self):
-    print "Found a packet on incoming port"
+def handler(pkt_in):
+    print "Found a packet on incoming port "+str(pkt_in)
     return  CONTINUE
 
 ##
@@ -83,22 +83,24 @@ def ws_install_flows(request, args):
     """ WS interface to permit(). """
     print args  
     if len(Switch_Delay_Test.st['ports']) < 1 :
-        return "{\"result\":0, reason: \"No switch yet joinned\"}"
+        return "{\"result\":0, \"reason\": \"No switch yet joinned\"}"
  
     for dpid in Switch_Delay_Test.st['ports'].keys():
         if not install_test_flows(dpid, args.get('flow_num'), args.get('flow_type')) :
-            return "{\"result\":0, reason: \"Invalid params\"}"
+            return "{\"result\":0, \"reason\": \"Invalid params\"}"
+
     return "{\"result\":1}"
     
 def install_test_flows(dpid, num, type):
     j=1
     wild= 0
     for i in range(int(num)):
+        i=i+1
         if(type == "exact") :
-            dst_ip = ("1.1.%d.%d"%(int((i/256) + 1),i%256))
+            dst_ip = ("10.3.%d.%d"%(int((i/256)),i%256))
             wild = 0
         elif (type == "wildcard"):
-            dst_ip = ("1.%d.%d.0"%(int((i/256) + 1),i%256))
+            dst_ip = ("10.%d.%d.0"%(int((i/256) + 3),i%256))
             wild = 8
         else :
             print "Invalid type"
@@ -108,12 +110,12 @@ def install_test_flows(dpid, num, type):
         Switch_Delay_Test.install_datapath_flow(dpid,
         { 
                 core.IN_PORT: 1,
-                core.DL_SRC: "11:11:11:11:11:11",
-                core.DL_DST: "22:11:11:11:11:11",
-                core.DL_VLAN: 65535,
+                core.DL_SRC: "10:20:30:40:50:61",
+                core.DL_DST: "10:20:30:40:50:60",
+                core.DL_VLAN: 0xffff,
                 core.DL_VLAN_PCP: 0,
                 core.DL_TYPE: ethernet.ethernet.IP_TYPE,
-                core.NW_SRC: "10.1.1.1",
+                core.NW_SRC: "10.2.0.1",
                 core.NW_DST: dst_ip,
                 core.NW_DST_N_WILD: wild,
                 core.NW_PROTO: ipv4.ipv4.UDP_PROTOCOL,
@@ -123,7 +125,9 @@ def install_test_flows(dpid, num, type):
                 }, 
         openflow.OFP_FLOW_PERMANENT, openflow.OFP_FLOW_PERMANENT,
         [
-                [openflow.OFPAT_OUTPUT, [-1, 1]]
+                [openflow.OFPAT_SET_DL_SRC, "10:20:30:40:50:60"],
+                [openflow.OFPAT_SET_DL_DST, "10:20:30:40:50:62"],
+                [openflow.OFPAT_OUTPUT, [-1,  openflow.OFPP_IN_PORT]]
          ],
         )
 
@@ -143,11 +147,32 @@ class switch_delay_test(core.Component):
         self.st = {}
         self.st['ports']={}
 
+
+    def pkt_in_handler(self, dp_id, inport, ofp_reason, total_frame_len, buffer_id, packet):
+        attrs = util.extract_flow(packet)
+        print "Pakcet in received "+str(inport)+ " " +packet_utils.ip_to_str(attrs['nw_dst'])
+        # print attrs
+
+        # print ("type:%s %s > %s  %s:%d > %s:%d "%(
+        #         packet_utils.ethtype_to_str(attrs['dl_type']),
+        #         packet_utils.mac_to_str(attrs['dl_src']),
+        #         packet_utils.mac_to_str(attrs['dl_dst']),
+        #         packet_utils.ip_to_str(attrs['nw_src']), attrs['tp_src'],
+        #         packet_utils.ip_to_str(attrs['nw_dst']), attrs['tp_dst']
+        #         ))
+        return CONTINUE
+        
     
     def install(self):
+        match = {core.DL_TYPE: ethernet.ethernet.IP_TYPE}
+
         Switch_Delay_Test.register_for_datapath_join(datapath_join)
         Switch_Delay_Test.register_for_datapath_leave(datapath_leave)
-        self.register_handler (Packet_in_event.static_get_name(), handler)
+        self.register_for_packet_match(lambda
+            dp,inport,reason,len,bid,packet :
+            switch_delay_test.pkt_in_handler(self,dp,inport,reason,len,bid,packet),
+            0xffff, match)
+        #self.register_handler (Packet_in_event.static_get_name(), handler)
 
         ws = self.resolve(str(webservice.webservice))
         v1 = ws.get_version("1")
